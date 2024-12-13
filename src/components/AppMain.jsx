99b0439ac634eabf838b71f8d611382ac2b40dab
TommasoVilla91 <tommasovilla@yahoo.it>
2024-12-13 16:03:40 +0100

    feat: milestone 1 completed

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,16 +1,29 @@
 import CardElement from "./CardElement";
+import posts from "../../data";
 import style from "./AppMain.module.css";
 
 function AppMain() {
+
+    const printPosts = () => {
+        return posts.map((curPost) => (
+            <div className="col" key={curPost.id}>
+                <CardElement 
+                    image={curPost.image}
+                    title={curPost.title}
+                    content={curPost.content}
+                    tags={curPost.tags}
+                    published={curPost.published}
+                />
+            </div>
+        ));
+    };
 
     return (
         <>
             <main className={style.main}>
                 <div className="container">
                     <div className="row">
-                        <div className="col">
-                            <CardElement />
-                        </div>
+                        {printPosts()}                        
                     </div>
                 </div>
             </main>        
--- src/components/CardElement.jsx
+++ src/components/CardElement.jsx
@@ -1,16 +1,23 @@
 import InfoButton from "./InfoButton";
 import style from "./CardElement.module.css";
 
-function CardElement() {
+function CardElement(props) {
 
     return (
         <>
             <div className={style.card}>
-                <div className={style.image}></div>
+                <div className={style.image}>
+                    {/* <img src={props.image}/> */}
+                </div>
                 <div className={style.text}>
-                    <h3 className={style.title}>Titolo del Post</h3>
-                    <p className={style.infos}>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam excepturi quaerat fugiat natus cupiditate corporis quo aspernatur nobis in animi inventore quis provident hic accusantium, tempora saepe deserunt, fugit ratione.
+                    <h3 className={style.title}>
+                        {props.title}
+                    </h3>
+                    <p className={style.content}>
+                        {props.content}
+                    </p>
+                    <p className={style.tags}>
+                        {props.tags.join(', ')}
                     </p>
                     <InfoButton />
                 </div>                
